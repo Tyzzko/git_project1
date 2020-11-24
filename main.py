7db1c6ec778eadaa5fa422e640aad13824e5d486
Tyzzko <70973519+Tyzzko@users.noreply.github.com>
2020-11-24 20:40:08 +0300

    1

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import Qt
+from UI import Ui_Dialog
+from random import randint
+import sys
 
 
-# Press the green button in the gutter to run the script.
+class MyWidget(Ui_Dialog, QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.qp = QPainter(self)
+        self.i = randint(1, 11)
+        self.setupUi(self)
+        self.qp.begin(self)
+        self.qp.setBrush(QColor(255, 255, 255, 0))
+        self.pushButton.clicked.connect(self.do)
+        self.qp.end()
+
+    def do(self):
+        for i in range(self.i):
+
+            x, y, a = randint(1, 951), randint(1, 751), randint(1, 476)
+            self.qp.drawEllipse(x, y, a, a)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
